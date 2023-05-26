5269dcfb6cc30005af1040d537f036e7b2f8043b
Ka-Na03 <kaito1129daku@gmail.com>
2023-05-26 21:26:03 +0900

    STEP3-4まで完了

--- learan-record/SQL_database/Internet_TV_Service/setup-sql/create_table.sql
+++ learan-record/SQL_database/Internet_TV_Service/setup-sql/create_table.sql
@@ -16,15 +16,6 @@ CREATE TABLE channels (
   name VARCHAR(50)  NOT NULL
 );
 
-CREATE TABLE time_slots(
-  PRIMARY KEY (id),
-  FOREIGN KEY (channel_id) REFERENCES channels(id),
-  id          INT       NOT NULL AUTO_INCREMENT,
-  channel_id  INT       NOT NULL,
-  start_time  DATETIME  NOT NULL,
-  end_time    DATETIME  NOT NULL
-);
-
 CREATE TABLE genres(
   PRIMARY KEY (id),
   UNIQUE (name),
@@ -75,14 +66,27 @@ CREATE TABLE episodes(
   release_date DATE         NOT NULL
 );
 
+CREATE TABLE time_slots(
+  PRIMARY KEY (id),
+  FOREIGN KEY (channel_id) REFERENCES channels(id),
+  FOREIGN KEY (episode_id) REFERENCES episodes(id),
+  id          INT       NOT NULL AUTO_INCREMENT,
+  channel_id  INT       NOT NULL,
+  episode_id    INT       NOT NULL,
+  start_time  DATETIME  NOT NULL,
+  end_time    DATETIME  NOT NULL
+);
+
 CREATE TABLE viewership(
   PRIMARY KEY (id),
   FOREIGN KEY (episode_id) REFERENCES episodes(id),
   FOREIGN KEY (time_slot_id) REFERENCES time_slots(id),
+  FOREIGN KEY (program_id) REFERENCES programs(id),
   FOREIGN KEY (channel_id) REFERENCES channels(id),
   id            INT NOT NULL AUTO_INCREMENT,
   episode_id    INT NOT NULL,
   time_slot_id  INT NOT NULL,
+  program_id    INT NOT NULL,
   channel_id    INT NOT NULL,
   view_count    INT DEFAULT 0
 );
